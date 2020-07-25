91380881b4820477ae06e3fe8a8ad43bbb1edf4d
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-07-25 10:13:15 -0300

    feat: create RootQuery structure

--- src/schema/scheme.js
+++ src/schema/scheme.js
@@ -1,4 +1,4 @@
-const { GraphQLObjectType, GraphQLString } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require('graphql');
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -8,3 +8,18 @@ const BookType = new GraphQLObjectType({
     genre: { type: GraphQLString },
   }),
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
