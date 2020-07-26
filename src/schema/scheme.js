bbf9b9d13da9b09011d1dbec89934174cffffff2
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-07-26 06:41:24 -0300

    feat: add hardcoded object as example for db

--- src/schema/scheme.js
+++ src/schema/scheme.js
@@ -1,5 +1,11 @@
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require('graphql');
 
+const books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
@@ -17,6 +23,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from db / other source
+        return books.find(book => book.id === args.id);
       },
     },
   },
