a341172a46bd4a1325c9f20cb0626c5c92774fde
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-07-26 07:30:41 -0300

    feat: create authors object to add relationships

--- src/schema/scheme.js
+++ src/schema/scheme.js
@@ -1,17 +1,44 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = require('graphql');
 
 const books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', author_id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', author_id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', author_id: '3' },
+];
+
+const authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
 ];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return authors.find(author => author.id === parent.author_id);
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -20,10 +47,16 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // code to get data from db / other source
         return books.find(book => book.id === args.id);
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return authors.find(author => author.id === args.id);
       },
     },
   },
