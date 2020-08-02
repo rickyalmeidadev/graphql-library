c64a224adb361384d97ec3e71246becb6090c812
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-02 20:19:38 -0300

    feat: set apollo client configs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import BooksList from './components/BooksList';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+});
+
 const App = () => (
-  <div>
-    <h1>GraphQL Library</h1>
-    <BooksList />
-  </div>
+  <ApolloProvider client={client}>
+    <div>
+      <h1>GraphQL Library</h1>
+      <BooksList />
+    </div>
+  </ApolloProvider>
 );
 
 export default App;
