666b54126a44c02f2b86168163e60d2f56e17f43
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-02 20:18:49 -0300

    feat: allow cors

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 require('./database/connection');
 
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
