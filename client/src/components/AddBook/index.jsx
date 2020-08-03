ff0cf217c17130fce0d6f479623e416733cb1025
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-02 21:01:46 -0300

    feat: make form fields controlled

--- client/src/components/AddBook/index.jsx
+++ client/src/components/AddBook/index.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../../queries/queries';
 
+const initialValues = {
+  name: '',
+  genre: '',
+  author: '',
+};
+
 const AddBook = ({ data }) => {
+  const [formData, setFormData] = useState(initialValues);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(formData);
+  };
+
   const renderOptions = () => {
     if (data.loading) {
       return <option disabled>Loading...</option>;
@@ -18,23 +37,43 @@ const AddBook = ({ data }) => {
   };
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <div className="form__group">
         <label htmlFor="name" className="label">
           Book name:
-          <input type="text" id="name" name="name" className="input" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="input"
+          />
         </label>
       </div>
       <div className="form__group">
         <label htmlFor="genre" className="label">
           Genre:
-          <input type="text" id="genre" name="genre" className="input" />
+          <input
+            type="text"
+            id="genre"
+            name="genre"
+            value={formData.genre}
+            onChange={handleChange}
+            className="input"
+          />
         </label>
       </div>
       <div className="form__group">
         <label htmlFor="author" className="label">
           Author:
-          <select name="author" id="author" className="input input--select">
+          <select
+            name="author"
+            id="author"
+            value={formData.author}
+            onChange={handleChange}
+            className="input input--select"
+          >
             {renderOptions()}
           </select>
         </label>
