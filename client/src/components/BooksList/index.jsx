46b6176cca3db29707efd884ba33bbefc98df1ce
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-05 09:51:58 -0300

    feat: apply BookDetails component and its logic

--- client/src/components/BooksList/index.jsx
+++ client/src/components/BooksList/index.jsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../../queries/queries';
+import BookDetails from '../BookDetails';
 
 const BooksList = ({ data }) => {
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  const handleSelectBook = (bookId) => {
+    setSelectedBook(bookId);
+  };
+
   const renderBooks = () => {
     if (data.loading) {
-      return <span>Loading...</span>;
+      return <span className="loading">Loading...</span>;
     }
 
     if (data.error) {
-      return <span>Failed to load books</span>;
+      return <span className="error">Failed to load books</span>;
     }
 
     return data.books.map(book => (
-      <li key={book.id}>{book.name}</li>
+      <li
+        key={book.id}
+        onClick={() => handleSelectBook(book.id)}
+        className="books__item"
+      >
+        {book.name}
+
+      </li>
     ));
   };
 
   return (
-    <div>
-      <ul>
+    <section className="books">
+      <ul className="books__list">
         {renderBooks()}
       </ul>
-    </div>
+      <BookDetails bookId={selectedBook} />
+    </section>
   );
 };
 
